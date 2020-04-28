8d76a52a5979d51a54d54a785a1a184bd9ec9bf9
Fabian Vilela <fabianvilela0@gmail.com>
2020-04-28 18:07:25 -0400

    updating masterscanner

--- masterscanner.sh
+++ masterscanner.sh
@@ -5,10 +5,12 @@ echo "This is the MASTERSCANNER"
 echo "Syntax: ./masterscanner.py <IP>"
 echo "EX: ./masterscanner.py 192.168.53.32"
 if [[ $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-	echo "goodjob"
-	nmap -T4 -p- $1 | grep ^[1-9] | tr -d ^{a-z} | tr -d / > port_num.txt
-	cat port_num.txt | while read line; do
-		nmap -T4 -A -p $line $1 >> portinfo.txt # much faster than running -A on all ports
+	echo "--------------------------------------------------------------------------------"
+	echo "                           NOW SCANNING $1"
+	echo "--------------------------------------------------------------------------------"
+	nmap -T4 -p- $1 | grep ^[1-9] | tr -d ^{a-z} | tr -d / > portNum.txt
+	cat portNum.txt | while read line; do
+		nmap -T4 -A -p $line $1 >> nmapscan.txt # much faster than running -A on all ports
 		if [[ $line == 80 || $line == 443 ]]; then
 			nikto -h $1 > niktofile.txt # running nikto against port 80(http)
 			fullstring="$httpnum$1"
@@ -21,7 +23,10 @@ if [[ $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
 			nmap --script http-rfi-spider -p $line $1 >> topnmapscriptresults.txt
 			nmap -p $line --script http-default-accounts $1 >> topnmapscriptresults.txt
 			nmap -p $line --script=ssl-cert $1 >> topnmapscriptresults.txt
+			# ABOVE IS A LIST OF NMAP SCRIPTS THAT ARE EFFECTIVE
+			#FEEL FREE TO ADD MORE ABOVE THIS LINE
+			theharvester -d $1 -b google -l 500 > harvesterresults.txt #harvesterscan 
 		fi
 	done
-	rm port_num.txt
+	rm portNum.txt
 fi
